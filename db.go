ea0b614aa80633d8c0937951a054e58d82a41106
User One <user.one@email.com>
2018-11-13 08:58:49 +0700

    db.go complete

--- db.go
+++ db.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"log"
 	"net/http"
 	"text/template"
 
@@ -34,9 +35,96 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		panic(err.Error())
 	}
 	cat := Category{}
-	res := []Employee{}
+	res := []Category{}
 	for selDB.Next() {
 		var id int
-		var name stringerr
+		var name string
+		err = selDB.Scan(&id, &name)
+		if err != nil {
+			panic(err.Error())
+		}
+		cat.id = id
+		cat.name = name
+		res = append(res, cat)
 	}
+	tmpl.ExecuteTemplate(w, "Index", res)
+	defer db.Close()
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	nId := r.URL.Query().Get("id")
+	selDB, err := db.Query("SELECT * FROM categories WHERE id=?", nId)
+	if err != nil {
+		panic(err.Error())
+	}
+	cat := Category{}
+	for selDB.Next() {
+		var id int
+		var name string
+		err = selDB.Scan(&id, &name)
+		if err != nil {
+			panic(err.Error())
+		}
+		cat.id = id
+		cat.name = name
+	}
+	tmpl.ExecuteTemplate(w, "Edit", emp)
+	defer db.Close()
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	if r.Method == "POST" {
+		name := r.FormValue("name")
+		insForm, err := db.Prepare("INSERT INTO categories(name) VALUES(?)")
+		if err != nil {
+			panic(err.Error())
+		}
+		insForm.Exec(name, city)
+		log.Println("INSERT: Name: " + name)
+	}
+	defer db.Close()
+	http.Redirect(w, r, "/", 301)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	if r.Method == "POST" {
+		name := r.FormValue("name")
+		id := r.FormValue("uid")
+		insForm, err := db.Prepare("UPDATE categories SET name=? WHERE id=?")
+		if err != nil {
+			panic(err.Error())
+		}
+		insForm.Exec(name, id)
+		log.Println("UPDATE: Name: " + name)
+	}
+	defer db.Close()
+	http.Redirect(w, r, "/", 301)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	emp := r.URL.Query().Get("id")
+	delForm, err := db.Prepare("DELETE FROM categories WHERE id=?")
+	if err != nil {
+		panic(err.Error())
+	}
+	delForm.Exec(emp)
+	log.Println("DELETE")
+	defer db.Close()
+	http.Redirect(w, r, "/", 301)
+}
+
+func main() {
+	log.Println("Server started on: http://localhost:8080")
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/show", Show)
+	http.HandleFunc("/new", New)
+	http.HandleFunc("/edit", Edit)
+	http.HandleFunc("/insert", Insert)
+	http.HandleFunc("/update", Update)
+	http.HandleFunc("/delete", Delete)
+	http.ListenAndServe(":8080", nil)
 }
